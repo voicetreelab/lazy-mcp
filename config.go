da637eed64e6784a180ce10dd17c04142a3a0b94
tbxark <tbxark@outlook.com>
2025-05-25 21:24:05 +0800

    refactor: Separate discarded configurations into a separate file

--- config.go
+++ config.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"time"
 
@@ -34,51 +33,12 @@ const (
 	MCPClientTypeStreamable MCPClientType = "streamable-http"
 )
 
-// ---- V1 ----
+type MCPServerType string
 
-type MCPClientConfigV1 struct {
-	Type           MCPClientType   `json:"type"`
-	Config         json.RawMessage `json:"config"`
-	PanicIfInvalid bool            `json:"panicIfInvalid"`
-	LogEnabled     bool            `json:"logEnabled"`
-	AuthTokens     []string        `json:"authTokens"`
-}
-
-type MCPProxyConfigV1 struct {
-	BaseURL          string   `json:"baseURL"`
-	Addr             string   `json:"addr"`
-	Name             string   `json:"name"`
-	Version          string   `json:"version"`
-	GlobalAuthTokens []string `json:"globalAuthTokens"`
-}
-
-func parseMCPClientConfigV1(conf *MCPClientConfigV1) (any, error) {
-	switch conf.Type {
-	case MCPClientTypeStdio:
-		var config StdioMCPClientConfig
-		err := json.Unmarshal(conf.Config, &config)
-		if err != nil {
-			return nil, err
-		}
-		return &config, nil
-	case MCPClientTypeSSE:
-		var config SSEMCPClientConfig
-		err := json.Unmarshal(conf.Config, &config)
-		if err != nil {
-			return nil, err
-		}
-		return &config, nil
-	case MCPClientTypeStreamable:
-		var config StreamableMCPClientConfig
-		err := json.Unmarshal(conf.Config, &config)
-		if err != nil {
-			return nil, err
-		}
-		return &config, nil
-	default:
-		return nil, errors.New("invalid client type")
-	}
-}
+const (
+	MCPServerTypeSSE        MCPServerType = "sse"
+	MCPServerTypeStreamable MCPServerType = "streamable-http"
+)
 
 // ---- V2 ----
 
@@ -102,11 +62,12 @@ type OptionsV2 struct {
 }
 
 type MCPProxyConfigV2 struct {
-	BaseURL string     `json:"baseURL"`
-	Addr    string     `json:"addr"`
-	Name    string     `json:"name"`
-	Version string     `json:"version"`
-	Options *OptionsV2 `json:"options,omitempty"`
+	BaseURL string        `json:"baseURL"`
+	Addr    string        `json:"addr"`
+	Name    string        `json:"name"`
+	Version string        `json:"version"`
+	Type    MCPServerType `json:"type,omitempty"`
+	Options *OptionsV2    `json:"options,omitempty"`
 }
 
 type MCPClientConfigV2 struct {
@@ -160,71 +121,22 @@ type Config struct {
 	McpServers map[string]*MCPClientConfigV2 `json:"mcpServers"`
 }
 
-func load(path string) (*Config, error) {
-	type FullConfig struct {
-		DeprecatedServerV1  *MCPProxyConfigV1             `json:"server"`
-		DeprecatedClientsV1 map[string]*MCPClientConfigV1 `json:"clients"`
+type FullConfig struct {
+	DeprecatedServerV1  *MCPProxyConfigV1             `json:"server"`
+	DeprecatedClientsV1 map[string]*MCPClientConfigV1 `json:"clients"`
 
-		McpProxy   *MCPProxyConfigV2             `json:"mcpProxy"`
-		McpServers map[string]*MCPClientConfigV2 `json:"mcpServers"`
-	}
+	McpProxy   *MCPProxyConfigV2             `json:"mcpProxy"`
+	McpServers map[string]*MCPClientConfigV2 `json:"mcpServers"`
+}
+
+func load(path string) (*Config, error) {
+
 	conf, err := confstore.Load[FullConfig](path)
 	if err != nil {
 		return nil, err
 	}
+	adaptMCPClientConfigV1ToV2(conf)
 
-	if conf.DeprecatedServerV1 != nil && conf.McpProxy == nil {
-		v1 := conf.DeprecatedServerV1
-		conf.McpProxy = &MCPProxyConfigV2{
-			BaseURL: v1.BaseURL,
-			Addr:    v1.Addr,
-			Name:    v1.Name,
-			Version: v1.Version,
-			Options: &OptionsV2{
-				AuthTokens: v1.GlobalAuthTokens,
-			},
-		}
-	}
-
-	if len(conf.DeprecatedClientsV1) > 0 && len(conf.McpServers) == 0 {
-		conf.McpServers = make(map[string]*MCPClientConfigV2)
-		for name, clientConfig := range conf.DeprecatedClientsV1 {
-			clientInfo, cErr := parseMCPClientConfigV1(clientConfig)
-			if cErr != nil {
-				continue
-			}
-			options := &OptionsV2{
-				AuthTokens: clientConfig.AuthTokens,
-			}
-			if conf.DeprecatedServerV1 != nil && len(conf.DeprecatedServerV1.GlobalAuthTokens) > 0 {
-				options.AuthTokens = append(options.AuthTokens, conf.DeprecatedServerV1.GlobalAuthTokens...)
-			}
-			switch v := clientInfo.(type) {
-			case *StdioMCPClientConfig:
-				conf.McpServers[name] = &MCPClientConfigV2{
-					Command: v.Command,
-					Args:    v.Args,
-					Env:     v.Env,
-					Options: options,
-				}
-			case *SSEMCPClientConfig:
-				conf.McpServers[name] = &MCPClientConfigV2{
-					URL:     v.URL,
-					Headers: v.Headers,
-					Options: options,
-				}
-			case *StreamableMCPClientConfig:
-				conf.McpServers[name] = &MCPClientConfigV2{
-					URL:     v.URL,
-					Headers: v.Headers,
-					Timeout: v.Timeout,
-					Options: options,
-				}
-			default:
-				continue
-			}
-		}
-	}
 	if conf.McpProxy == nil {
 		return nil, errors.New("mcpProxy is required")
 	}
@@ -246,9 +158,9 @@ func load(path string) (*Config, error) {
 		}
 	}
 
-	// remove deprecated fields
-	conf.DeprecatedServerV1 = nil
-	conf.DeprecatedClientsV1 = nil
+	if conf.McpProxy.Type == "" {
+		conf.McpProxy.Type = MCPServerTypeSSE // default to SSE
+	}
 
 	return &Config{
 		McpProxy:   conf.McpProxy,
